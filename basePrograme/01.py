97258a5e2b3c9fa98de7607a7e6c44a59db9c56f
littlepaike <f335125303@163.com>
2022-01-18 20:28:14 +0800

    zip

--- basePrograme/01.py
+++ basePrograme/01.py
@@ -6,3 +6,19 @@ print(result)
 
 a = (20, 10, 30, 9, 8)
 print(a[3])
+
+# 元组切片
+a = tuple("abcdefghijklmnopqrstuvwxyz")
+print(a)
+print(a[1:10:2])
+print(a[::-1])
+print(max(a))
+
+
+#zip
+a = [10,20,30]
+b = [40,50,60]
+c = [70,80,90]
+d = zip(a,b,c)
+print(d)
+print(list(d))
