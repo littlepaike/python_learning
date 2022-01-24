2f16cb17240a1acc57df9d0a06d3af145b56dd12
littlepaike <f335125303@163.com>
2022-01-25 00:45:02 +0800

    第八章  文件操作

--- 第二季/第八章 文件操作/递归目录.py
+++ 第二季/第八章 文件操作/递归目录.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 # 使用递归计算n的阶乘
+import os
+
 
 def factorial(n):
     if n == 1:
@@ -11,6 +13,12 @@ def factorial(n):
 print(factorial(5))
 
 
+def getAllFiles(path):
+    childFiles = os.listdir(path)
+    for file in childFiles:
+        filepath = os.path.join(path, file)
+        if os.path.isdir(filepath):
+            getAllFiles(filepath)
+        print(filepath)
 
-
-
+getAllFiles("movie")
